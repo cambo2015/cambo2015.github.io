673b3af9b5465553f2901399634f5e357b5301e2
cambo2015 <cameronhansen91@outlook.com>
2019-02-09 16:21:12 -0700

    typo fix

--- playground.js
+++ playground.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-canvas.width = window.innerWidth;
+var canvas   = document.getElementById("canvas");
+var context  = canvas.getContext("2d");
+canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
